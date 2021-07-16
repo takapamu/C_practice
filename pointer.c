028adbc572cb4d87c521316d0057f6b6f3a79f78
takapamu <takapamukyary0129@gmail.com>
2021-07-16 23:33:47 +0900

    pointer.c

---
+++ pointer.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void
+funcA(int i)
+{
+  i = 7;
+}
+
+void
+funcB(int *i)
+{
+  (*i) = 222;
+}
+
+int main()
+{
+  int a = 1;
+
+  printf("a = %d\n" , a);
+
+  funcA(a);
+  printf("after funcA() : a = %d\n", a);
+
+  funcB(&a);
+  printf("after funcB() : a = %d\n", a);
+
+  return 0;
+
+}
