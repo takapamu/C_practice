110fa370172b27e6e57d3e4dd7c2eb0f3a3c700b
takapamu <takapamukyary0129@gmail.com>
2021-07-16 23:15:33 +0900

    circle.c

---
+++ circle.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+const float pi = 3.14;
+
+float calc_a(float r){
+  float area = pi * r * r;
+  return area;
+}
+
+float calc_l(float r){
+  float len = pi * 2 * r;
+  return len;
+}
+
+void prt(float r, float f1, float f2){
+  printf("半径%.1fの円の面積は%.2f、円周の長さは%.2f\n", r, f1, f2);
+}
+
+int main(void){
+  float r = 20.0f, ans = 0, len = 0;
+  ans = calc_a(r);
+  len = calc_l(r);
+  prt(r, ans, len);
+
+  return 0;
+}
