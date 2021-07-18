33925ab1ee57590ba28a4a36722281f1d9873e86
takapamu <takapamukyary0129@gmail.com>
2021-07-18 22:46:46 +0900

    graph.c

---
+++ graph.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+FILE *gp;
+
+gp = popen("gnuplot -persist", "w"); // パイプを開き、gnuplotの立ち上げ
+
+fprintf(gp, "plot sin(x)\n"); // パイプを通してgnuplotにコマンドを送る
+
+fprintf(gp, "exit\n"); // gnuplotの終了
+fflush(gp);
+pclose(gp); // パイプを閉じる
+
+return 0;
+}
